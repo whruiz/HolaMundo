11c489f2fd2ce03707a1f0e7867bea4f573045b5
whruiz <whruiz@gmail.com>
2021-09-23 22:55:25 -0500

    Se creó el registro Persona en main

---
+++ src/com/acme/PersonaRegistro.java
@@ -0,0 +1,9 @@
+    package com.acme;
+
+    public record PersonaRegistro(String nombre, String apellidos) {
+
+        //@Override
+        //public String toString() {
+        //    return "%s %s".formatted(nombre, apellidos);
+        //}
+    }
